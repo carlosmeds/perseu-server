dd22116f42fb0675e3607fafe9cfa0fe89e8d836
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-09-25 19:05:55 -0300

    refactro: improve createTraining

--- src/infra/postgres/repo/TrainingRepo.ts
+++ src/infra/postgres/repo/TrainingRepo.ts
@@ -6,44 +6,45 @@ import { Training } from "../schema/Training.schema";
 
 export class TrainingRepo {
   async createTraining(athletes: any, sessions: any) {
-    const result = await AppDataSource.transaction(async (transactionalEntityManager) => {
-      const sessionsSaved = await Promise.all(
-        sessions.map(async (session: any) => {
-          const exercises = await Promise.all(
-            session.exercises.map(async (exercise: any) => {
-              const { name, description } = exercise;
-              const exerciseSave = new Exercise();
-              exerciseSave.name = name;
-              exerciseSave.description = description;
-              return await transactionalEntityManager.save(
-                exerciseSave
-              );
-            })
-          );
-          const sessionSave = new Session();
-          sessionSave.name = session.name;
-          sessionSave.exercises = exercises;
-          return await transactionalEntityManager.save(sessionSave);
-        })
-      );
+    const result = await AppDataSource.transaction(
+      async (transactionalEntityManager) => {
+        const sessionsSaved = await Promise.all(
+          sessions.map(async (session: any) => {
+            const exercises = await Promise.all(
+              session.exercises.map(async (exercise: any) => {
+                const { name, description } = exercise;
+                const exerciseSave = new Exercise();
+                exerciseSave.name = name;
+                exerciseSave.description = description;
 
-      const athletesSaved = await Promise.all(
-        athletes.map(async (id: any) => {
-          const result = await transactionalEntityManager.findOneBy(Athlete, {
-            id: id,
-          });
-          return result;
-        })
-      );
-      const training = new Training();
+                return await transactionalEntityManager.save(exerciseSave);
+              })
+            );
+            const sessionSave = new Session();
+            sessionSave.name = session.name;
+            sessionSave.exercises = exercises;
 
-      training.athletes = athletesSaved;
-      training.sessions = sessionsSaved;
-      training.sessions = sessionsSaved;
-      await transactionalEntityManager.save(training);
-      return training;
-    });
+            return await transactionalEntityManager.save(sessionSave);
+          })
+        );
 
-    return result
+        const athletesSaved = await Promise.all(
+          athletes.map(async (id: any) => {
+            const result = await transactionalEntityManager.findOneBy(Athlete, {
+              id: id,
+            });
+            return result;
+          })
+        );
+        
+        const training = new Training();
+        training.athletes = athletesSaved;
+        training.sessions = sessionsSaved;
+
+        return await transactionalEntityManager.save(training);
+      }
+    );
+
+    return result;
   }
 }
