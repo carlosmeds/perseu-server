aa6c5e04beb654e62ea851ac153cfee24da603a8
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-09-25 19:27:28 -0300

    feat: add typeorm variables

--- src/infra/postgres/data-source.ts
+++ src/infra/postgres/data-source.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { TYPEORM_HOST, TYPEORM_PORT, TYPEORM_USERNAME, TYPEORM_PASSWORD, TYPEORM_DATABASE, TYPEORM_SYNCHRONIZE } from "../../main/config/env";
 import { Athlete } from "./schema/Athlete.schema";
 import { Coach } from "./schema/Coach.schema";
 import { Exercise } from "./schema/Exercise.schema";
@@ -11,12 +12,12 @@ import { User } from "./schema/User.schema";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "test",
-  synchronize: true,
+  host: TYPEORM_HOST,
+  port: Number(TYPEORM_PORT),
+  username: TYPEORM_USERNAME,
+  password: TYPEORM_PASSWORD,
+  database: TYPEORM_DATABASE,
+  synchronize: Boolean(TYPEORM_SYNCHRONIZE),
   logging: false,
   entities: [Athlete, User, Coach, Team, Request, Exercise, Session, Training],
   migrations: [],
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -6,3 +6,11 @@ dotenv.config();
 dotenv.config({ path: `${__dirname}/../../.env` });
 
 export const { JWT_SECRET } = env;
+export const {
+  TYPEORM_HOST,
+  TYPEORM_PORT,
+  TYPEORM_USERNAME,
+  TYPEORM_PASSWORD,
+  TYPEORM_DATABASE,
+  TYPEORM_SYNCHRONIZE,
+} = env;
