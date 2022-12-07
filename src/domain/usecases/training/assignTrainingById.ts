af0fa4e6e8952fb66de754fd93e8813642376317
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-12-06 22:39:01 -0300

    feat: check if athlete already has training

--- src/domain/usecases/training/assignTrainingById.ts
+++ src/domain/usecases/training/assignTrainingById.ts
@@ -10,21 +10,31 @@ export class AssignTrainingByIdUseCase {
     private athleteTrainingRepo: AthleteTrainingRepo
   ) {}
 
-  async execute(id: number, athletes: string[]): Promise<any> {    
+  async execute(id: number, athletes: string[]): Promise<any> {
     const training = await this.trainingRepo.getTrainingById(Number(id));
     if (!training) {
       return notFound("Treino não encontrado");
     }
 
     await Promise.all(
-      athletes.map(async (id: string) => {
-        const result = await this.athleteRepo.getAthlete(Number(id));
-        if (result) {
-          await this.athleteTrainingRepo.assignTrainingById([result], training);
+      athletes.map(async (athleteId: string) => {
+        const athlete = await this.athleteRepo.getAthlete(Number(athleteId));
+
+        const athleteTraining =
+          await this.athleteTrainingRepo.getAthleteTraining(
+            Number(athleteId),
+            id
+          );
+        if (athleteTraining) return;
+
+        if (athlete) {
+          await this.athleteTrainingRepo.assignTrainingById(
+            [athlete],
+            training
+          );
         }
       })
     );
-
 
     return success({ message: "Treino atribuído com sucesso" });
   }
